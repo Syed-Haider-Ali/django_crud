1050dbb5c18e570ee8ecd644cb2aa97fce6eb9b6
Syed Haider Ali <s.haider0303@gmail.com>
2023-08-30 12:30:32 +0500

    Optimized: Get_Vechile:API

--- vechile/serializer.py
+++ vechile/serializer.py
@@ -2,15 +2,14 @@ from rest_framework.serializers import ModelSerializer
 from .models import Make, Vechile
 
 
-
 class VechileSerializer(ModelSerializer):
     class Meta:
         model = Vechile
         fields ='__all__'
 
     def to_representation(self, instance):
-        data = super(VechileSerializer, self).to_representation(instance)
-        data["make"] = MakeSerializer(instance.make).data
+        data = super().to_representation(instance)
+        data["make"] = MakeListingSerializer(instance.make).data
         return data
 
 
@@ -18,3 +17,9 @@ class MakeSerializer(ModelSerializer):
     class Meta:
         model = Make
         fields ='__all__'
+
+
+class MakeListingSerializer(ModelSerializer):
+    class Meta:
+        model = Make
+        fields = ('id', 'name')
